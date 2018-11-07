59d4913a445e0c39c18b9021277f9d5ac84e8be0
Jakub Sokołowski <jakub@status.im>
2018-11-07 13:16:32 +0100

    fix module names in outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "public_ips" {
   value = [
     "${module.do-ams3.public_ips}",
-    "${module.gc-us-central1.public_ips}",
+    "${module.gc-us-central1-a.public_ips}",
     "${module.ac-cn-hongkong-c.public_ips}"
   ]
 }
@@ -9,7 +9,7 @@ output "public_ips" {
 output "hosts" {
   value = {
     "do-ams3"          = "${module.do-ams3.hosts}"
-    "gc-us-central1-1" = "${module.gc-us-central1.hosts}"
+    "gc-us-central1-1" = "${module.gc-us-central1-a.hosts}"
     "ac-cn-hongkong-c" = "${module.ac-cn-hongkong-c.hosts}"
   }
 }
