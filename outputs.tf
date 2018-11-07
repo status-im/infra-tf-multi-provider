c00ef9cddcb10bf1553765878feb8be2eeaf7abd
Jakub Sokołowski <jakub@status.im>
2018-11-07 13:08:59 +0100

    add a hosts output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
 output "public_ips" {
   value = [
     "${module.do-ams3.public_ips}",
-    "${module.gc-us-central1-a.public_ips}",
-    "${module.ac-cn-hongkong-c.public_ips}",
+    "${module.gc-us-central1.public_ips}",
+    "${module.ac-cn-hongkong-c.public_ips}"
   ]
+}
+
+output "hosts" {
+  value = {
+    "do-ams3"          = "${module.do-ams3.hosts}"
+    "gc-us-central1-1" = "${module.gc-us-central1.hosts}"
+    "ac-cn-hongkong-c" = "${module.ac-cn-hongkong-c.hosts}"
+  }
 }
