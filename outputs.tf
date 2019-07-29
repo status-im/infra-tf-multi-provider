b6e7a703027c651e93f4c6737a385b66e75be184
Jakub Sokołowski <jakub@status.im>
2019-07-28 20:31:04 -0400

    upgradte to Terraform 0.12
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,16 @@
 output "public_ips" {
   value = [
-    "${module.do-eu-amsterdam3.public_ips}",
-    "${module.gc-us-central1-a.public_ips}",
-    "${module.ac-cn-hongkong-c.public_ips}"
+    module.do-eu-amsterdam3.public_ips,
+    module.gc-us-central1-a.public_ips,
+    module.ac-cn-hongkong-c.public_ips,
   ]
 }
 
 output "hosts" {
   value = {
-    "do-eu-amsterdam3" = "${module.do-eu-amsterdam3.hosts}"
-    "gc-us-central1-a" = "${module.gc-us-central1-a.hosts}"
-    "ac-cn-hongkong-c" = "${module.ac-cn-hongkong-c.hosts}"
+    "do-eu-amsterdam3" = module.do-eu-amsterdam3.hosts
+    "gc-us-central1-a" = module.gc-us-central1-a.hosts
+    "ac-cn-hongkong-c" = module.ac-cn-hongkong-c.hosts
   }
 }
+
