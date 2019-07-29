08ffff33dbd9f1f1098c339fe829d16d85ebf00c
Jakub Sokołowski <jakub@status.im>
2019-07-28 22:56:42 -0400

    add a separate hosts_by_dc output, use hosts for just a list
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,17 @@ output "public_ips" {
 }
 
 output "hosts" {
+  value = merge(
+    module.do-eu-amsterdam3.hosts,
+    module.gc-us-central1-a.hosts,
+    module.ac-cn-hongkong-c.hosts,
+  )
+}
+
+output "hosts_by_dc" {
   value = {
     "do-eu-amsterdam3" = module.do-eu-amsterdam3.hosts
     "gc-us-central1-a" = module.gc-us-central1-a.hosts
     "ac-cn-hongkong-c" = module.ac-cn-hongkong-c.hosts
   }
 }
-
