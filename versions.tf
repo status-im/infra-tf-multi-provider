12191483f509323fd5625c3640c5964224f3fac0
Jakub Sokołowski <jakub@status.im>
2021-09-24 11:47:10 +0200

    versions: drop specifying cloud provider versions
    They are taken from submodules anyway.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -2,21 +2,18 @@
 terraform {
   required_version = "~> 1.0.0"
   required_providers {
-    alicloud = {
-      source  = "aliyun/alicloud"
-      version = " = 1.124.3"
-    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = " = 2.21.0"
     }
+    alicloud = {
+      source  = "aliyun/alicloud"
+    }
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = " = 2.9.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = " = 3.73.0"
     }
   }
 }
