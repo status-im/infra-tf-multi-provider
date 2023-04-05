58397ff4bfa7aeebba9948ad8b08c1da228f3387
Jakub Sokołowski <jakub@status.im>
2023-04-05 09:13:26 +0200

    versions: use looser restriction for terrform
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -1,11 +1,12 @@
 
 terraform {
-  required_version = "~> 1.3.0"
+  required_version = "> 1.3.0"
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
     }
     alicloud = {
+
       source  = "aliyun/alicloud"
     }
     digitalocean = {
